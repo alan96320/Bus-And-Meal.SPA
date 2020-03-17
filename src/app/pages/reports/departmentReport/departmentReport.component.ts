09a6f424d87de3f43a5c1076e29b35fce7d14bac
alan365 <acepalan3@gmail.com>
2020-03-17 11:14:26 +0700

    fixed

--- src/app/pages/reports/departmentReport/departmentReport.component.ts
+++ src/app/pages/reports/departmentReport/departmentReport.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from "@angular/core";
-import { ViewEncapsulation } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { DepartmentService } from "src/app/_services/department.service";
-import { ActivatedRoute } from "@angular/router";
-import { SweetAlertService } from "src/app/_services/sweetAlert.service";
-import { Department } from "src/app/_models/department";
+import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DepartmentService } from 'src/app/_services/department.service';
+import { ActivatedRoute } from '@angular/router';
+import { SweetAlertService } from 'src/app/_services/sweetAlert.service';
+import { Department } from 'src/app/_models/department';
 
 declare var Stimulsoft: any;
 
 @Component({
-  selector: "app-departmentReport",
-  templateUrl: "./departmentReport.component.html",
-  styleUrls: ["./departmentReport.component.css"]
+  // tslint:disable-next-line:component-selector
+  selector: 'app-departmentReport',
+  templateUrl: './departmentReport.component.html',
+  styleUrls: ['./departmentReport.component.css']
 })
 export class DepartmentReportComponent implements OnInit {
   departments: Department[];
@@ -24,23 +25,23 @@ export class DepartmentReportComponent implements OnInit {
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.departments = data["department"];
+      this.departments = data.department;
     });
 
     const report = Stimulsoft.Report.StiReport.createNewReport();
     const options = new Stimulsoft.Viewer.StiViewerOptions();
-    report.loadFile("../assets/reports/Department.mrt");
-    report.dictionary.variables.getByName("title").valueObject =
-      "Department List";
+    report.loadFile('../assets/reports/Department.mrt');
+    report.dictionary.variables.getByName('title').valueObject =
+      'Department List';
 
-    report.regData("Department", "Department", this.departments);
+    report.regData('Department', 'Department', this.departments);
 
-    options.width = "100%";
-    options.height = "850px";
+    options.width = '100%';
+    options.height = '850px';
     options.appearance.scrollbarsMode = true;
 
-    const viewer = new Stimulsoft.Viewer.StiViewer(options, "StiViewer", false);
+    const viewer = new Stimulsoft.Viewer.StiViewer(options, 'StiViewer', false);
     viewer.report = report;
-    viewer.renderHtml("departmentReport");
+    viewer.renderHtml('departmentReport');
   }
 }
