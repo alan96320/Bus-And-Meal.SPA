69c2650f26d5e6522836a224b3dc8b73db200082
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2020-03-17 11:03:24 +0700

    revise meal type report

--- src/app/pages/reports/mealTypeReport/mealTypeReport.component.ts
+++ src/app/pages/reports/mealTypeReport/mealTypeReport.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { MealType } from 'src/app/_models/mealType';
-import { MealTypeService } from 'src/app/_services/mealType.service';
-import { ActivatedRoute } from '@angular/router';
-import { SweetAlertService } from 'src/app/_services/sweetAlert.service';
+import { Component, OnInit } from "@angular/core";
+import { MealType } from "src/app/_models/mealType";
+import { MealTypeService } from "src/app/_services/mealType.service";
+import { ActivatedRoute } from "@angular/router";
+import { SweetAlertService } from "src/app/_services/sweetAlert.service";
 
 declare var Stimulsoft: any;
 
 @Component({
   // tslint:disable-next-line:component-selector
-  selector: 'app-mealTypeReport',
-  templateUrl: './mealTypeReport.component.html',
-  styleUrls: ['./mealTypeReport.component.css']
+  selector: "app-mealTypeReport",
+  templateUrl: "./mealTypeReport.component.html",
+  styleUrls: ["./mealTypeReport.component.css"]
 })
 export class MealTypeReportComponent implements OnInit {
   mealTypes: any = [];
@@ -28,25 +28,25 @@ export class MealTypeReportComponent implements OnInit {
 
     let mealTypeData = [];
     mealTypeData = JSON.parse(JSON.stringify(this.mealTypes));
-    mealTypeData.map(data => {
-      data.vendorname = data.mealVendor.name;
-      delete data.mealVendor;
-    });
+    // mealTypeData.map(data => {
+    //   data.vendorname = data.mealVendor.name;
+    //   delete data.mealVendor;
+    // });
 
     const report = Stimulsoft.Report.StiReport.createNewReport();
     const options = new Stimulsoft.Viewer.StiViewerOptions();
-    report.loadFile('../assets/reports/MealType.mrt');
-    report.dictionary.variables.getByName('title').valueObject =
-      'Meal Type List';
+    report.loadFile("../assets/reports/MealType.mrt");
+    report.dictionary.variables.getByName("title").valueObject =
+      "Meal Type List";
 
-    report.regData('MealType', 'MealType', mealTypeData);
+    report.regData("MealType", "MealType", mealTypeData);
 
-    options.width = '100%';
-    options.height = '850px';
+    options.width = "100%";
+    options.height = "850px";
     options.appearance.scrollbarsMode = true;
 
-    const viewer = new Stimulsoft.Viewer.StiViewer(options, 'StiViewer', false);
+    const viewer = new Stimulsoft.Viewer.StiViewer(options, "StiViewer", false);
     viewer.report = report;
-    viewer.renderHtml('mealtypeReport');
+    viewer.renderHtml("mealtypeReport");
   }
 }
