e52298c797d4d0a628709f98895db91e827984db
alan365 <acepalan3@gmail.com>
2020-03-19 16:59:55 +0700

    fixed report

--- src/app/pages/reports/mealOrderReport/mealOrderReport.component.ts
+++ src/app/pages/reports/mealOrderReport/mealOrderReport.component.ts
@@ -107,45 +107,45 @@ export class MealOrderReportComponent implements OnInit {
   }
 
   loadReport() {
+    this.orderDetail = [];
+    this.mealtype = [];
     this.mealOrderEntryService.getMealOrderReports(this.mealOrderReortParams).subscribe((res: PaginatedResult<MealOrder[]>) => {
-          this.mealOrderResource = res.result;
-          // this.pagination = res.pagination;
-          this.mealOrderResource.mealTypeResult.map(mt => {
-        this.mealtype.push({ id: mt.id, name: mt.name });
-      });
-
-          this.mealOrderResource.mealOrderResult.map(mo => {
-        mo.mealOrderDetails.map(mod => {
-          this.orderDetail.push({
-            date: this.convertDate(mo.orderEntryDate),
-            department: this.mealOrderResource.departmentResult.find(
-              d => d.id === mo.departmentId
-            ).name,
-            mealtypeid: mod.mealTypeId,
-            total: mod.orderQty,
-            collected: mo.mealOrderVerificationId > 0 ? 'Yes' : 'No'
+      this.mealOrderResource = res.result;
+      this.mealOrderResource.mealTypeResult.map(mt => {
+          this.mealtype.push({ id: mt.id, name: mt.name });
+        });
+      this.mealOrderResource.mealOrderResult.map(mo => {
+          mo.mealOrderDetails.map(mod => {
+            this.orderDetail.push({
+              date: this.convertDate(mo.orderEntryDate),
+              department: this.mealOrderResource.departmentResult.find(
+                d => d.id === mo.departmentId
+              ).name,
+              mealtypeid: mod.mealTypeId,
+              total: mod.orderQty,
+              collected: mo.mealOrderVerificationId > 0 ? 'Yes' : 'No'
+            });
           });
         });
-      });
 
-          const report = Stimulsoft.Report.StiReport.createNewReport();
-          const options = new Stimulsoft.Viewer.StiViewerOptions();
-          report.loadFile('../assets/reports/MealOrder.mrt');
-          report.dictionary.variables.getByName('title').valueObject =
+      const report = Stimulsoft.Report.StiReport.createNewReport();
+      const options = new Stimulsoft.Viewer.StiViewerOptions();
+      report.loadFile('../assets/reports/MealOrder.mrt');
+      report.dictionary.variables.getByName('title').valueObject =
         'Meal Order List';
 
-          this.mealOrderReport.orderdetail = this.orderDetail;
-          this.mealOrderReport.mealtype = this.mealtype;
-          report.dictionary.databases.clear();
-          report.regData('MealOrder', 'MealOrder', this.mealOrderReport);
+      this.mealOrderReport.orderdetail = this.orderDetail;
+      this.mealOrderReport.mealtype = this.mealtype;
+      report.dictionary.databases.clear();
+      report.regData('MealOrder', 'MealOrder', this.mealOrderReport);
 
-          options.width = '100%';
-          options.height = '850px';
-          options.appearance.scrollbarsMode = true;
+      options.width = '100%';
+      options.height = '850px';
+      options.appearance.scrollbarsMode = true;
 
-          const viewer = new Stimulsoft.Viewer.StiViewer(options, 'StiViewer', false);
-          viewer.report = report;
-          viewer.renderHtml('mealOrderReport');
+      const viewer = new Stimulsoft.Viewer.StiViewer(options, 'StiViewer', false);
+      viewer.report = report;
+      viewer.renderHtml('mealOrderReport');
         },
         error => {
           this.sweetAlert.error(error);
@@ -168,9 +168,6 @@ export class MealOrderReportComponent implements OnInit {
 
   ngOnInit() {
     this.loadDepartment();
-    // this.route.data.subscribe(data => {
-    //   this.mealOrderResource = data.mealorder;
-    // });
     this.loadReport();
 
 
