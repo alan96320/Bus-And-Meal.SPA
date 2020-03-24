88886c5f8953e75a437e716b34abfc15c1358156
alan365 <acepalan3@gmail.com>
2020-03-24 12:51:49 +0700

    bus order entry e=and verification reort

--- src/app/pages/reports/busOrderReport/busOrderReport.component.ts
+++ src/app/pages/reports/busOrderReport/busOrderReport.component.ts
@@ -7,9 +7,10 @@ import { ReportService } from 'src/app/_services/report.service';
 import { PaginatedResult } from 'src/app/_models/pagination';
 import { BusOrder } from 'src/app/_models/busOrder';
 import { environment } from 'src/environments/environment';
+import { ConvertDateService } from 'src/app/_services/convertDate.service';
 
 declare var Stimulsoft: any;
-
+declare var $: any;
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'app-busOrderReport',
@@ -44,56 +45,36 @@ export class BusOrderReportComponent implements OnInit {
   ];
 
   constructor(
-    private route: ActivatedRoute,
-    private calendar: NgbCalendar,
     public formatter: NgbDateParserFormatter,
     private http: HttpClient,
     private sweetAlert: SweetAlertService,
-    private reportService: ReportService
-  ) {
-    this.fromDate = calendar.getToday();
-    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
-  }
+    private reportService: ReportService,
+    private converDate: ConvertDateService
+  ) { }
 
-  onDateSelection(date: NgbDate) {
-    if (!this.fromDate && !this.toDate) {
-      this.fromDate = date;
-    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
-      this.toDate = date;
-    } else {
-      this.toDate = null;
-      this.fromDate = date;
-    }
-  }
-
-  isHovered(date: NgbDate) {
-    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
-  }
-
-  isInside(date: NgbDate) {
-    return date.after(this.fromDate) && date.before(this.toDate);
-  }
-
-  isRange(date: NgbDate) {
-    return date.equals(this.fromDate) || date.equals(this.toDate) || this.isInside(date) || this.isHovered(date);
-  }
-
-  validateInput(currentValue: NgbDate, input: string): NgbDate {
-    const parsed = this.formatter.parse(input);
-    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
+  ngOnInit() {
+    const start = $('#start');
+    const end = $('#end');
+    start.datepicker({
+      format: 'dd-mm-yyyy',
+      autoHide: true
+    });
+    end.datepicker({
+      format: 'dd-mm-yyyy',
+      autoHide: true
+    });
+    this.loadDepartment();
+    this.loadReport();
+    $('#department').change(function() {
+      $(this).blur();
+    });
   }
 
   Print(depart) {
-    // console.log();
-    const startDate = this.fromDate.year + '-' + this.fromDate.month + '-' + this.fromDate.day;
-    const endDate = this.toDate.year + '-' + this.toDate.month + '-' + this.toDate.day;
-    const department = depart;
-    this.busOrderReportParams.startDate = startDate;
-    this.busOrderReportParams.endDate = endDate;
-    this.busOrderReportParams.department = department;
+    this.busOrderReportParams.startDate = this.converDate.convertAB($('#start').val());
+    this.busOrderReportParams.endDate = this.converDate.convertAB($('#end').val());
+    this.busOrderReportParams.department = depart;
     this.loadReport();
-
-
   }
 
   loadDepartment() {
@@ -151,10 +132,5 @@ export class BusOrderReportComponent implements OnInit {
       this.monthName[d.getMonth()],
       d.getFullYear()
     ].join(' ');
-  }
-
-  ngOnInit() {
-    this.loadDepartment();
-    this.loadReport();
   }
 }
--- src/app/pages/reports/busVerificationReport/busVerificationReport.component.ts
+++ src/app/pages/reports/busVerificationReport/busVerificationReport.component.ts
@@ -7,9 +7,10 @@ import { ReportService } from 'src/app/_services/report.service';
 import { BusOrderVerification } from 'src/app/_models/busOrderVerification';
 import { PaginatedResult } from 'src/app/_models/pagination';
 import { environment } from 'src/environments/environment';
+import { ConvertDateService } from 'src/app/_services/convertDate.service';
 
 declare var Stimulsoft: any;
-
+declare var $: any;
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'app-busVerificationReport',
@@ -43,43 +44,29 @@ export class BusVerificationReportComponent implements OnInit {
   ];
 
   constructor(
-    private route: ActivatedRoute,
-    private calendar: NgbCalendar,
     public formatter: NgbDateParserFormatter,
     private http: HttpClient,
     private sweetAlert: SweetAlertService,
-    private reportService: ReportService
-  ) {
-    this.fromDate = calendar.getToday();
-    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
-  }
+    private reportService: ReportService,
+    private converDate: ConvertDateService
+  ) {}
 
-  onDateSelection(date: NgbDate) {
-    if (!this.fromDate && !this.toDate) {
-      this.fromDate = date;
-    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
-      this.toDate = date;
-    } else {
-      this.toDate = null;
-      this.fromDate = date;
-    }
-  }
-
-  isHovered(date: NgbDate) {
-    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
-  }
-
-  isInside(date: NgbDate) {
-    return date.after(this.fromDate) && date.before(this.toDate);
-  }
-
-  isRange(date: NgbDate) {
-    return date.equals(this.fromDate) || date.equals(this.toDate) || this.isInside(date) || this.isHovered(date);
-  }
-
-  validateInput(currentValue: NgbDate, input: string): NgbDate {
-    const parsed = this.formatter.parse(input);
-    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
+  ngOnInit() {
+    const start = $('#start');
+    const end = $('#end');
+    start.datepicker({
+      format: 'dd-mm-yyyy',
+      autoHide: true
+    });
+    end.datepicker({
+      format: 'dd-mm-yyyy',
+      autoHide: true
+    });
+    this.loadDepartment();
+    this.loadReport();
+    $('#department').change(function() {
+      $(this).blur();
+    });
   }
 
   loadDepartment() {
@@ -91,13 +78,9 @@ export class BusVerificationReportComponent implements OnInit {
   }
 
   Print(depart) {
-    // console.log();
-    const startDate = this.fromDate.year + '-' + this.fromDate.month + '-' + this.fromDate.day;
-    const endDate = this.toDate.year + '-' + this.toDate.month + '-' + this.toDate.day;
-    const department = depart;
-    this.busVerificationReportParams.startDate = startDate;
-    this.busVerificationReportParams.endDate = endDate;
-    this.busVerificationReportParams.department = department;
+    this.busVerificationReportParams.startDate = this.converDate.convertAB($('#start').val());
+    this.busVerificationReportParams.endDate = this.converDate.convertAB($('#end').val());
+    this.busVerificationReportParams.department = depart;
     this.loadReport();
   }
 
@@ -147,10 +130,5 @@ export class BusVerificationReportComponent implements OnInit {
       this.monthName[d.getMonth()],
       d.getFullYear()
     ].join(' ');
-  }
-
-  ngOnInit() {
-    this.loadDepartment();
-    this.loadReport();
   }
 }
